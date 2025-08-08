1a872110564dff486b6a8851e8fb52493bf51a90
Takhir <guvalakat1603@gmail.com>
2025-08-08 16:18:40 +0500

    done

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into books(title, author)
-values
-    ('Spring Security - data.sql', 'John Doe'),
-    ('Spring Cloud - data.sql', 'Josh Long');
---
+++ src/main/resources/db/h2/data-h2.sql
@@ -0,0 +1,4 @@
+insert into books(title, author)
+values
+    ('Spring Security - data-postgresql.sql', 'John Doe'),
+    ('Spring Cloud - data-postgresql.sql', 'Josh Long');
