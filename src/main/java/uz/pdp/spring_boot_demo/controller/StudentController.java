8354e89ff3e6111685b873c9310e2aa37565b642
Takhir <guvalakat1603@gmail.com>
2025-08-13 17:59:02 +0500

    jpa

--- src/main/java/uz/pdp/spring_boot_demo/controller/StudentController.java
+++ src/main/java/uz/pdp/spring_boot_demo/controller/StudentController.java
@@ -1,13 +1,14 @@
 package uz.pdp.spring_boot_demo.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 import uz.pdp.spring_boot_demo.entity.Student;
 import uz.pdp.spring_boot_demo.repository.StudentRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/student")
@@ -23,6 +24,20 @@ public class StudentController {
     public ResponseEntity<List<Student>> findAll(){
         List<Student> allStudents = studentRepository.findAll();
         return ResponseEntity.ok(allStudents);
+    }
+
+    @GetMapping("/findById/{id}")
+    public ResponseEntity<Student> findById(@PathVariable Integer id){
+        Optional<Student> optional = studentRepository.findById(id);
+        return optional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(404).body(null));
+    }
+
+    @PostMapping("/create")
+    @Transactional
+    public ResponseEntity<Void> create(@RequestBody Student student){
+        System.out.println(student);
+        studentRepository.save(student);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
